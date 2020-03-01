dc8fc5cdbd96511b768ec97a0688bdc2404075fe
Tiger Pang <42376447+TigerPang@users.noreply.github.com>
2020-03-01 21:16:27 +1100

    added question description

--- Easy/35. Search Insert Position.py
+++ Easy/35. Search Insert Position.py
@@ -2,13 +2,13 @@ class Solution:
     def searchInsert(self, nums, target):
         l , r = 0, len(nums)-1
         while l <= r:
-            mid=(l+r)//2
-            if nums[mid]== target:
+            mid=(l + r) // 2
+            if nums[mid] == target:
                 return mid
             if nums[mid] < target:
-                l = mid+1
+                l = mid + 1
             else:
-                r = mid-1
+                r = mid - 1
         return l
 #         if target in nums:
 #             return nums.index(target)
@@ -20,3 +20,16 @@ class Solution:
 #                     return nums.index(i)
 if __name__ == '__main__':
     print(Solution().searchInsert([1,2,3], 6))
+    
+    
+    """
+        Time Complexity = O(N)
+        Space Complexity = O(1)
+
+        Given a sorted array and a target value, return the index if the target is found.
+        If not, return the index where it would be if it were inserted in order.
+        You may assume no duplicates in the array.
+        Example:
+        Input: [1,3,5,6], 5
+        Output: 2
+    """
